e4610fc0dd2eb18c23e961e76b9f1c42b5e57941
Adam Hsu <adam_hsu@comcast.com>
2020-07-04 12:44:44 -0700

    goweb 22/51

--- goweb/tronics/tronics.go
+++ goweb/tronics/tronics.go
@@ -2,8 +2,8 @@ package tronics
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo"
 	"gopkg.in/go-playground/validator.v9"
 )
@@ -11,19 +11,34 @@ import (
 var e = echo.New()
 var v = validator.New()
 
+func init() {
+	err := cleanenv.ReadEnv(&cfg)
+	fmt.Printf("%+v", cfg)
+	if err != nil {
+		e.Logger.Fatal("Unable to load configuration")
+	}
+}
+
+func serverMessage(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Println("inside middleware")
+		return next(c)
+	}
+}
+
+// type MiddlewareFunc func(HandlerFunc) HandlerFunc
+// type HandlerFunc func(Context) error
+
 //Start starts the application
 func Start() {
-	port := os.Getenv("MY_APP_PORT")
-	if port == "" {
-		port = "8080"
-	}	
-
+	// e.Use(serverMessage)
+	// e.Pre(serverMessage)
 	e.GET("/products", getProducts)
-	e.GET("products/:id", getProducts)
+	e.GET("products/:id", getProduct)
 	e.DELETE("products/:id", deleteProduct)
 	e.PUT("/product/:id", updateProduct)
 	e.POST("/products", createProduct)
 
-	e.Logger.Print(fmt.Sprintf("Listening on port %s", port))
-	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
+	e.Logger.Print(fmt.Sprintf("Listening on port %s", cfg.Port))
+	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", cfg.Port)))
 }
